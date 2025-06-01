62a27e94976bbf53260b8798eb219849a614b859
Andrii Omelchenko <andreyomem@gmail.com>
2025-06-01 14:47:40 +0300

    implement classes and create db

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
+import json
+
 import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json", "r") as input_file:
+        players = json.load(input_file)
+    for username, player_data in players.items():
+        race_data = player_data["race"]
+        race, boolean = Race.objects.get_or_create(
+            name=race_data["name"],
+            description=race_data.get("description", "")
+        )
+
+        for skill in race_data["skills"]:
+            Skill.objects.get_or_create(
+                name=skill["name"],
+                bonus=skill["bonus"],
+                race=race
+            )
+
+        guild_data = player_data["guild"]
+        if guild_data:
+            guild, boolean = Guild.objects.get_or_create(
+                name=guild_data["name"],
+                description=guild_data["description"]
+            )
+        else:
+            guild = None
+
+        Player.objects.get_or_create(nickname=username,
+                                     email=player_data["email"],
+                                     bio=player_data["bio"],
+                                     race=race,
+                                     guild=guild
+                                     )
 
 
 if __name__ == "__main__":
